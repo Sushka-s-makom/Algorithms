e329c4766d0c3bb5b575013df24f072e77934bf9
Sushka-s-makom <zabrodskiyalex111206@gmail.com>
2026-01-17 20:52:47 +0300

    test : add test and __main__ example

--- algorthms_on_python/strings/equal.py
+++ algorthms_on_python/strings/equal.py
@@ -23,6 +23,22 @@ def equal(A: str, B: str) -> bool:
             return False
 
     return True
+def test_equal():
+    print("test_case #1:", end=" ")
+    print("Ok" if equal("abc", "abc") else "Fail")
+
+    print("test_case #2:", end=" ")
+    print("Ok" if not equal("abc", "ab") else "Fail")
+
+    print("test_case #3:", end=" ")
+    print("Ok" if not equal("abc", "abd") else "Fail")
+
+    print("test_case #4:", end=" ")
+    print("Ok" if equal("", "") else "Fail")
+
+    print("test_case #5:", end=" ")
+    print("Ok" if not equal("abc", "abC") else "Fail")
 
 
-
+if __name__ == "__main__":
+    test_equal()
