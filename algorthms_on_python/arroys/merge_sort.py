6cec1ca8b098e2e3fbdaaaeee683eac5f73a93b6
Sushka-s-makom <zabrodskiyalex111206@gmail.com>
2026-01-17 20:02:51 +0300

    test: add basic assert self-tests and __main__ examples

--- algorthms_on_python/arroys/merge_sort.py
+++ algorthms_on_python/arroys/merge_sort.py
@@ -32,5 +32,24 @@ def merge_sort(A):
     return merge(L, R)
 
 
+def _is_sorted(x):
+    return all(x[i] <= x[i + 1] for i in range(len(x) - 1))
 
+
+def _self_test():
+    assert merge_sort([]) == []
+    assert merge_sort([1]) == [1]
+    assert merge_sort([2, 1]) == [1, 2]
+    assert merge_sort([3, 2, 1]) == [1, 2, 3]
+    assert merge_sort([5, 1, 5, 2, 0]) == [0, 1, 2, 5, 5]
+
+    out = merge_sort([10, -1, 2, 2, 9, 0])
+    assert _is_sorted(out)
+
+    print("OK: merge_sort self_test")
+
+
+if __name__ == "__main__":
+    print(merge_sort([5, 2, 9, 1, 5, 6]))
+    _self_test()
 
