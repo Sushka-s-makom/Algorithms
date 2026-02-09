6dd783ea094d13819eb81a0174cd64f73ed5e940
Sushka-s-makom <zabrodskiyalex111206@gmail.com>
2026-02-10 00:48:15 +0300

    chore : add fast polinom hash func

--- algorthms_on_python/hash/rolling hash.py
+++ algorthms_on_python/hash/rolling hash.py
@@ -8,3 +8,17 @@ def calc_hash(data):
         i += 1
         data //= 2
     return s % 2 ** 32
+
+def fast_calc_hash(data):
+    k = 3571
+    MOD = 2 ** 32
+    data += 8233293
+    s = 0
+    p = k
+    while data > 0:
+        bit = data & 1  # быстрее чем data % 2
+        s = (s + bit * p) % MOD
+        p = (p * k) % MOD  # следующий k**(i+1)
+        data >>= 1  # быстрее чем data //= 2
+    return s
+
