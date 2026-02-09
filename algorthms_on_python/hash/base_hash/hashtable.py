1b99539afbd5cf36b3b6872060c82e5741fb2837
Sushka-s-makom <zabrodskiyalex111206@gmail.com>
2026-02-10 01:41:33 +0300

    chore : add set, get, delete funcs  for hash table

--- algorthms_on_python/hash/base_hash/hashtable.py
+++ algorthms_on_python/hash/base_hash/hashtable.py
@@ -12,3 +12,38 @@ def ht_index(ht, key):
     h = fast_calc_hash(key)
     return h % ht["m"]
 
+def ht_set(ht, key, value):
+    idx = ht_index(ht, key)
+    bucket = ht["buckets"][idx]
+
+    # если ключ уже есть - обновляем
+    for i, (k, _) in enumerate(bucket):
+        if k == key:
+            bucket[i] = (key, value)
+            return
+    #иначе
+    bucket.append((key, value))
+    ht["n"] += 1
+
+def ht_get(ht, key, default=None):
+    idx = ht_index(ht, key)
+    bucket = ht["buckets"][idx]
+
+    for k, v in bucket:
+        if k == key:
+            return v
+    return default
+
+def ht_delete(ht, key: int) -> bool:
+    idx = ht_index(ht, key)
+    bucket = ht["buckets"][idx]
+
+    for i, (k, _) in enumerate(bucket):
+        if k == key:
+            bucket.pop(i)
+            ht["n"] -= 1
+            return True
+    return False
+
+
+
